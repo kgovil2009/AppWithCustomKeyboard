98c64918397a14f82a500729c91bf686a6f626f1
kgovil2009 <56232912+kgovil2009@users.noreply.github.com>
2021-04-24 03:47:15 -0500

    custom keyboard

--- app/src/main/java/com/example/customkeyboard/MainActivity.java
+++ app/src/main/java/com/example/customkeyboard/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Date;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,12 +31,27 @@ public class MainActivity extends AppCompatActivity {
         editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
         editText.setTextIsSelectable(true);
 
+        String stringsToType[] = {
+                "The quick brown fox jumps over the lazy dog",
+                "The plump wizard converted junk boxes to quality feed bags",
+                "Jodie very quickly examined and sewed the big fine zippers",
+                "The jay, pig, fox, zebra and my wolves quack",
+                "A wizard job is to vex chumps quickly in fog",
+                "The five boxing wizards jump quickly",
+                "Pack my box with five dozen liquor jugs",
+                "Two driven jocks help fax my big quiz"
+
+        };
+
         // pass the InputConnection from the EditText to the keyboard
         InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
         keyboard.setInputConnection(ic);
 
         ImageButton startButton;
+        EditText typedData;
+        Button newStringButton;
         ImageButton endButton;
+        TextView dataToType;
         TextView finalData;
         TextView finalErrorData;
         final long[] startTime = {0};
@@ -47,12 +63,25 @@ public class MainActivity extends AppCompatActivity {
         endButton = (ImageButton) findViewById(R.id.stopButton);
         finalData = (TextView) findViewById(R.id.final_data);
         finalErrorData = (TextView) findViewById(R.id.final_data2);
+        dataToType = (TextView) findViewById(R.id.text_view_id);
+        newStringButton = (Button) findViewById(R.id.new_string);
+        typedData = (EditText) findViewById(R.id.editText);
 
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 startTime[0] = System.currentTimeMillis();
+
+                if(dataToType.getVisibility() == View.INVISIBLE)
+                {
+
+                    Random random = new Random();
+                    int index = random.nextInt(stringsToType.length - 0) + 0;
+                    dataToType.setText(stringsToType[index]);
+                    dataToType.setVisibility(View.VISIBLE);
+                }
             }
         });
         endButton.setOnClickListener(new View.OnClickListener() {
@@ -60,10 +89,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 endTime[0] = System.currentTimeMillis();
                 timeTaken[0] = endTime[0] - startTime[0];
-                dataToBeDisplayed[0] = "Time Taken: " + timeTaken[0] + "ms";
+                dataToBeDisplayed[0] = "Time Taken: " + timeTaken[0]/1000.0 + "s";
                 finalData.setVisibility(View.VISIBLE);
                 finalData.setText(dataToBeDisplayed[0]);
                 finalErrorData.setVisibility(View.VISIBLE);
+                newStringButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        newStringButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                typedData.setText("");
+                dataToType.setVisibility(View.INVISIBLE);
+                finalData.setVisibility(View.INVISIBLE);
+                finalErrorData.setVisibility(View.INVISIBLE);
+                newStringButton.setVisibility(View.INVISIBLE);
             }
         });
 
